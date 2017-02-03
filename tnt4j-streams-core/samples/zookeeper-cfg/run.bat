589827c96d9133883703b090a26084d7763aee39
akausinis <andrius.kausinis@singleton-labs.lt>
2017-02-03 15:10:47 +0200

    ZK configuration use sample

--- tnt4j-streams-core/samples/zookeeper-cfg/run.bat
+++ tnt4j-streams-core/samples/zookeeper-cfg/run.bat
@@ -1,1 +1,1 @@
-..\..\bin\tnt4j-streams.bat -z:stream-zk.properties
+..\..\bin\tnt4j-streams.bat -z:stream-zk.properties -f:../single-log/tnt-data-source.xml
--- tnt4j-streams-core/samples/zookeeper-cfg/run.sh
+++ tnt4j-streams-core/samples/zookeeper-cfg/run.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-../../bin/tnt4j-streams -z:stream-zk.properties
+../../bin/tnt4j-streams -z:stream-zk.properties -f:../single-log/tnt-data-source.xml
