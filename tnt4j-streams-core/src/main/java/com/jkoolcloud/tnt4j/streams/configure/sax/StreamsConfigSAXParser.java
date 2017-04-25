18d1474f41eb4e957ffe5404e174cdfdaf8a65bc
akausinis <andrius.kausinis@singleton-labs.lt>
2017-04-25 10:43:18 +0300

    config XML validation against XSD.

--- tnt4j-streams-core/src/main/java/com/jkoolcloud/tnt4j/streams/configure/sax/StreamsConfigSAXParser.java
+++ tnt4j-streams-core/src/main/java/com/jkoolcloud/tnt4j/streams/configure/sax/StreamsConfigSAXParser.java
@@ -19,7 +19,7 @@ package com.jkoolcloud.tnt4j.streams.configure.sax;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Properties;
+import java.util.*;
 
 import javax.xml.XMLConstants;
 import javax.xml.parsers.ParserConfigurationException;
@@ -30,11 +30,18 @@ import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
 
+import org.apache.commons.collections4.MapUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.xml.sax.ErrorHandler;
 import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
 
+import com.jkoolcloud.tnt4j.core.OpLevel;
+import com.jkoolcloud.tnt4j.sink.DefaultEventSinkFactory;
+import com.jkoolcloud.tnt4j.sink.EventSink;
 import com.jkoolcloud.tnt4j.streams.configure.StreamsConfigData;
+import com.jkoolcloud.tnt4j.streams.utils.StreamsResources;
 import com.jkoolcloud.tnt4j.streams.utils.Utils;
 
 /**
@@ -43,6 +50,7 @@ import com.jkoolcloud.tnt4j.streams.utils.Utils;
  * @version $Revision: 2 $
  */
 public final class StreamsConfigSAXParser {
+	private static final EventSink LOGGER = DefaultEventSinkFactory.defaultEventSink(StreamsConfigSAXParser.class);
 
 	private static final String HANDLER_PROP_KEY = "tnt4j.streams.config.sax.handler";
 
@@ -68,7 +76,19 @@ public final class StreamsConfigSAXParser {
 			throws ParserConfigurationException, SAXException, IOException {
 		if (validate) {
 			config = config.markSupported() ? config : new ByteArrayInputStream(IOUtils.toByteArray(config));
-			validate(config);
+
+			Map<OpLevel, List<SAXParseException>> validationErrors = validate(config);
+
+			if (MapUtils.isNotEmpty(validationErrors)) {
+				for (Map.Entry<OpLevel, List<SAXParseException>> vee : validationErrors.entrySet()) {
+					for (SAXParseException ve : vee.getValue()) {
+						LOGGER.log(OpLevel.WARNING,
+								StreamsResources.getString(StreamsResources.RESOURCE_BUNDLE_NAME,
+										"StreamsConfigSAXParser.xml.validation.error"),
+								ve.getLineNumber(), ve.getColumnNumber(), vee.getKey(), ve.getLocalizedMessage());
+					}
+				}
+			}
 		}
 
 		Properties p = Utils.loadPropertiesResource("sax.properties"); // NON-NLS
@@ -98,21 +118,51 @@ public final class StreamsConfigSAXParser {
 	 *
 	 * @param config
 	 *            {@link InputStream} to get configuration data from
+	 * @return map of found validation errors
 	 * @throws SAXException
 	 *             if there was an error parsing the configuration
 	 * @throws IOException
 	 *             if there is an error reading the configuration data
 	 */
-	public static void validate(InputStream config) throws SAXException, IOException {
+	public static Map<OpLevel, List<SAXParseException>> validate(InputStream config) throws SAXException, IOException {
+		final Map<OpLevel, List<SAXParseException>> validationErrors = new HashMap<>();
 		try {
 			SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
 			Schema schema = factory.newSchema();
 			Validator validator = schema.newValidator();
+			validator.setErrorHandler(new ErrorHandler() {
+				@Override
+				public void warning(SAXParseException exception) throws SAXException {
+					handleValidationError(OpLevel.WARNING, exception);
+				}
+
+				@Override
+				public void error(SAXParseException exception) throws SAXException {
+					handleValidationError(OpLevel.ERROR, exception);
+				}
+
+				@Override
+				public void fatalError(SAXParseException exception) throws SAXException {
+					handleValidationError(OpLevel.FATAL, exception);
+				}
+
+				private void handleValidationError(OpLevel level, SAXParseException exception) {
+					List<SAXParseException> lErrorsList = validationErrors.get(level);
+					if (lErrorsList == null) {
+						lErrorsList = new ArrayList<>();
+						validationErrors.put(level, lErrorsList);
+					}
+
+					lErrorsList.add(exception);
+				}
+			});
 			validator.validate(new StreamSource(config));
 		} finally {
 			if (config.markSupported()) {
 				config.reset();
 			}
 		}
+
+		return validationErrors;
 	}
 }
