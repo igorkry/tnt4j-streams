534e7b5559c7d29889aeb6118d8f2dc83befb340
rnikula <rnikula@users.noreply.github.com>
2018-03-16 15:03:21 -0500

    allow override of tnt4j and log4j file location

--- bin/tnt4j-streams.sh
+++ bin/tnt4j-streams.sh
@@ -6,8 +6,18 @@ else
 fi
 
 LIBPATH="$LIBPATH:$SCRIPTPATH/../*:$SCRIPTPATH/../lib/*"
-TNT4JOPTS="-Dtnt4j.config=$SCRIPTPATH/../config/tnt4j.properties"
-LOG4JOPTS="-Dlog4j.configuration=file:$SCRIPTPATH/../config/log4j.properties"
+# tnt4j file override
+if [ -z "$TNT4J_PROPERTIES" ]; then
+  TNT4J_PROPERTIES="$SCRIPTPATH/../config/tnt4j.properties"
+fi
+TNT4JOPTS="-Dtnt4j.config=$TNT4J_PROPERTIES"
+
+# log4j file override
+if [ -z "$TNT4J_LOG4J_PROPERTIES" ]; then
+  TNT4J_LOG4J_PROPERTIES="$SCRIPTPATH/../config/log4j.properties"
+fi
+LOG4JOPTS="-Dlog4j.configuration=file:$TNT4J_LOG4J_PROPERTIES"
+
 #LOGBACKOPTS="-Dlogback.configurationFile=file:$SCRIPTPATH/../config/logback.xml"
 STREAMSOPTS="$STREAMSOPTS $LOG4JOPTS $TNT4JOPTS"
 
