078811cf3d658ec8806a9c36b3d2f0bfe899f1aa
Andrius Kausinis <andrius.kausinis@singleton-labs.lt>
2018-06-05 08:21:27 +0300

    kafka version bumped-up. message headers support added.

---
+++ tnt4j-streams-kafka/src/main/java/com/jkoolcloud/tnt4j/streams/utils/KafkaUtils.java
@@ -0,0 +1,138 @@
+/*
+ * Copyright 2014-2018 JKOOL, LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.jkoolcloud.tnt4j.streams.utils;
+
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.kafka.common.header.Header;
+import org.apache.kafka.common.header.Headers;
+
+/**
+ * General utility methods used by TNT4J-Streams Kafka module.
+ * 
+ * @version $Revision: 1 $
+ */
+public class KafkaUtils {
+	/**
+	 * Resolves {@link org.apache.kafka.common.header.Headers} contained header value defined by <tt>headers</tt>
+	 * keys/indices <tt>path<tt> array.
+	 *
+	 * @param path
+	 *            keys/indices path as array of consumer record headers
+	 * @param headers
+	 *            consumer record headers to resolve value
+	 * @param i
+	 *            processed locator path element index
+	 * @return resolved consumer record headers value, or {@code null} if value is not resolved
+	 * @throws java.lang.RuntimeException
+	 *             if field can't be found or accessed
+	 *
+	 * @see #getHeadersValue(String[], Iterable, int)
+	 */
+	public static Object getHeadersValue(String[] path, Headers headers, int i) throws RuntimeException {
+		if (ArrayUtils.isEmpty(path) || headers == null) {
+			return null;
+		}
+
+		Object val = null;
+		String headerStr = path[i];
+		try {
+			val = resolveHeaderByIndex(headers, Integer.parseInt(headerStr));
+
+			if (val != null) {
+				val = Utils.getFieldValue(path, val, i + 1);
+			}
+		} catch (NumberFormatException exc) {
+			Iterable<Header> hHeaders = headers.headers(headerStr);
+
+			if (hHeaders != null && i < path.length - 1) {
+				val = getHeadersValue(path, hHeaders, i + 1);
+			}
+		}
+
+		return val;
+	}
+
+	/**
+	 * Resolves {@link org.apache.kafka.common.header.Header} value from <tt>headers</tt> collection defined by
+	 * keys/indices <tt>path<tt> array.
+	 * <p>
+	 * If path does not terminate at header value level, then remaining path is processed using resolved header value as
+	 * Java object (POJO) over {@link Utils#getFieldValue(String[], Object, int)}.
+	 *
+	 * @param path
+	 *            keys/indices path as array of consumer record headers
+	 * @param headers
+	 *            consumer record headers collection to resolve value
+	 * @param i
+	 *            processed locator path element index
+	 * @return resolved consumer record header value, or {@code null} if value is not resolved
+	 * @throws java.lang.RuntimeException
+	 *             if field can't be found or accessed
+	 *
+	 * @see Utils#getFieldValue(String[], Object, int)
+	 */
+	public static Object getHeadersValue(String[] path, Iterable<Header> headers, int i) throws RuntimeException {
+		if (ArrayUtils.isEmpty(path) || headers == null) {
+			return null;
+		}
+
+		Object val = null;
+		String headerStr = path[i];
+		try {
+			val = resolveHeaderByIndex(headers, Integer.parseInt(headerStr));
+		} catch (NumberFormatException exc) {
+			for (Header header : headers) {
+				if (header.key().equals(headerStr)) {
+					val = header.value();
+					break;
+				}
+			}
+		}
+
+		if (val != null) {
+			val = Utils.getFieldValue(path, val, i + 1);
+		}
+
+		return val;
+	}
+
+	/**
+	 * Resolves consumer record header value defined by header index <tt>hIdx</tt>.
+	 * <p>
+	 * Value gets resolved only if <tt>headers</tt> is not {@code null} and <tt>hIdx</tt> is less than size of
+	 * <tt>headers</tt> collection.
+	 *
+	 * @param headers
+	 *            consumer record headers collection to resolve value
+	 * @param hIdx
+	 *            header index to get value
+	 * @return resolved consumer record header value, or {@code null} if value is not resolved
+	 */
+	public static Object resolveHeaderByIndex(Iterable<Header> headers, int hIdx) {
+		if (headers != null) {
+			int idx = 0;
+			for (Header header : headers) {
+				if (idx == hIdx) {
+					return header.value();
+				}
+				idx++;
+			}
+		}
+
+		return null;
+	}
+}
